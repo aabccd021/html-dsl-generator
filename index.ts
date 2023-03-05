b37461ba4b2dffd4f4026120247ecd833e6ef1aa
aabccd021 <aabccd021@gmail.com>
2023-03-05 07:23:24 +0700

    fix: bun

--- index.ts
+++ index.ts
@@ -1,7 +1,1 @@
-import { html } from './aab.js';
-import { withLiveReload } from './reload.js';
-
-// eslint-disable-next-line import/no-default-export
-export default withLiveReload({
-  fetch: () => new Response(html, { headers: { 'Content-Type': 'text/html' } }),
-});
+export const aabccd = 'g';
