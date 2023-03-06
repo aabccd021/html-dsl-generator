99d17df9dcd076453d82982f326dc307bde1db2b
aabccd021 <aabccd021@gmail.com>
2023-03-06 16:32:36 +0700

    fix: generate success

--- src/bun/generate.ts
+++ src/bun/generate.ts
@@ -1,11 +1,15 @@
-// eslint-disable-next-line import/no-nodejs-modules
+/* eslint-disable import/no-nodejs-modules */
+import { resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 import * as es from '../es/generate.js';
 
 const env: es.Env = {
-  writeFile: (fileUrl, fileContent) => () => Bun.write(fileUrl, fileContent),
-  absoluteScriptPath: fileURLToPath(import.meta.url),
+  writeFile: (fileUrl, fileContent) => () => {
+    console.log(fileUrl);
+    return Bun.write(fileUrl, fileContent);
+  },
+  absoluteScriptPath: resolve(fileURLToPath(import.meta.url), '..'),
 };
 
 export const generate = es.generate(env);
